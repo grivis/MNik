99fde714651d09135ef56b292ba48aed6e9964e5
grigory <sss>
2016-12-23 20:38:37 +0300

    Initial commit

---
+++ Test1.py
@@ -0,0 +1,12 @@
+text = 'Бородинское сражение произошло 7 сентября 1812'
+#counters = ("1","2","3","4","5","6","7","8","9","0")
+counters = set('0123456789')
+i =0
+for letter in text:
+    try:
+        i += int(letter)
+    except ValueError:
+        continue
+
+
+print(i)
