3554b906a7f51bfd79d398f12b7fe902f30ea483
grivis <semenov.grigoriy@gmail.com>
2017-01-13 19:38:17 +0300

    Использование Google API

---
+++ Google_Geocode.py
@@ -0,0 +1,17 @@
+##### https://github.com/googlemaps/google-maps-services-python
+# https://developers.google.com/places/
+
+import googlemaps
+
+
+gmaps = googlemaps.Client(key='AIzaSyAdFmqaYcof2P1b7UqaCFahdWJ03wismcY')
+
+# Geocoding an address
+geocode_result = gmaps.geocode(address='улица Каховка, дом 31, корпус 1',
+                               language='ru')
+for item in geocode_result[0]['address_components']:
+    print(item)
+# print(geocode_result[0]['address_components'][0])
+# print(geocode_result[0]['address_components'][1])
+print('Широта места', geocode_result[0]['geometry']['location']['lat'])
+print('Долгота места', geocode_result[0]['geometry']['location']['lng'])
